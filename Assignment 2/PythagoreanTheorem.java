a2bc07b55e9fb6951c7a4fe3a1dc4bcb5efc389f
Zhaozhe Wu <49044541+wzhaozhe@users.noreply.github.com>
2023-03-24 16:39:15 -0400

    Update PythagoreanTheorem.java

--- Assignment 2/PythagoreanTheorem.java
+++ Assignment 2/PythagoreanTheorem.java
@@ -1,6 +1,6 @@
 /*
  * File: PythagoreanTheorem.java
- * Name: 
+ * Name: Zhaozhe Wu
  * Section Leader: 
  * -----------------------------
  * This file is the starter file for the PythagoreanTheorem problem.
